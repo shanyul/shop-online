bac8dac6d84c88371952965e0348e2ea231d8fa9
vagrant <vagrant@homestead>
2018-10-10 06:39:41 +0000

    商品类目管理后台

--- database/admin.sql
+++ database/admin.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-09-30 14:04:29'),(2,0,6,'系统管理','fa-tasks',NULL,NULL,'2018-10-09 14:40:20'),(3,2,7,'管理员','fa-users','auth/users',NULL,'2018-10-09 14:40:20'),(4,2,8,'角色','fa-user','auth/roles',NULL,'2018-10-09 14:40:20'),(5,2,9,'权限','fa-ban','auth/permissions',NULL,'2018-10-09 14:40:20'),(6,2,10,'菜单','fa-bars','auth/menu',NULL,'2018-10-09 14:40:20'),(7,2,11,'操作日志','fa-history','auth/logs',NULL,'2018-10-09 14:40:20'),(8,0,2,'用户管理','fa-user','/users','2018-09-30 14:32:07','2018-10-09 09:40:13'),(9,0,3,'商品管理','fa-cubes','/products','2018-09-30 15:15:24','2018-10-09 09:40:13'),(10,0,4,'订单管理','fa-rmb','/orders','2018-10-08 18:00:10','2018-10-09 09:40:13'),(11,0,5,'优惠券管理','fa-tags','/coupon_codes','2018-10-09 14:40:09','2018-10-09 14:40:20');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-09-30 14:04:29'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,'2018-10-10 13:57:21'),(3,2,8,'管理员','fa-users','auth/users',NULL,'2018-10-10 13:57:21'),(4,2,9,'角色','fa-user','auth/roles',NULL,'2018-10-10 13:57:21'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,'2018-10-10 13:57:21'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,'2018-10-10 13:57:21'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,'2018-10-10 13:57:21'),(8,0,2,'用户管理','fa-user','/users','2018-09-30 14:32:07','2018-10-09 09:40:13'),(9,0,4,'商品管理','fa-cubes','/products','2018-09-30 15:15:24','2018-10-10 13:57:33'),(10,0,5,'订单管理','fa-rmb','/orders','2018-10-08 18:00:10','2018-10-10 13:57:33'),(11,0,6,'优惠券管理','fa-tags','/coupon_codes','2018-10-09 14:40:09','2018-10-10 13:57:33'),(12,0,3,'类目管理','fa-bars','/categories','2018-10-10 13:57:14','2018-10-10 13:57:33');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-09  9:17:39
+-- Dump completed on 2018-10-10  6:32:32
