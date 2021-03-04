ff1f5f66fef09cea122c75155d3f60373fa6ddef
shanyul <l>
2021-03-04 09:25:37 +0800

    随机拒绝

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
         Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
         Route::get('installments/{installment}/wechat', 'InstallmentsController@payByWechat')->name('installments.wechat');
-        Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
+        Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store')->middleware('random_drop:80');
     });
 });
 
